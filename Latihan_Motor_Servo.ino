3c2a815f99edc1c4eca57fc44bd685c17b9f327a
Naela <naelaayu4@gmail.com>
2025-10-23 15:55:25 +0700

    first commit

---
+++ Latihan_Motor_Servo.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo motorServo;
+#define pinMotorServo 3
+#define pinButton 4
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Program Motor Servo");
+
+  motorServo.attach(pinMotorServo);
+
+  motorServo.write(0);
+
+  pinMode(pinButton, INPUT_PULLUP);
+}
+
+void loop() {
+  
+  if (digitalRead(pinButton) == HIGH) {
+  Serial.println("Putar 360 Derajat");
+  motorServo.write(360);
+
+  }else {
+
+    Serial.println("Putar 0 Derajat");
+    motorServo.write(0);
+  }
+  delay(100);
+
+}
